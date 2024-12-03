7ccb5f8cc7f7fb29050d2e538b9655545a7527ca
alsqja <alsqha2626@gmail.com>
2024-12-03 21:24:00 +0900

    feat: DataInitializer 생성
    - user, store, order 더미 데이터 주입
    
    #7

---
+++ src/main/java/com/example/burrrrng/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package com.example.burrrrng.repository;
+
+import com.example.burrrrng.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
