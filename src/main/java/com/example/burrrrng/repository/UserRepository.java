9246d560e8e2890a202aef4e16e3be0fdd63d35d
MinSeok3796 <ms3796@naver.com>
2024-12-04 10:13:01 +0900

    Update UserRepository

--- src/main/java/com/example/burrrrng/repository/UserRepository.java
+++ src/main/java/com/example/burrrrng/repository/UserRepository.java
@@ -2,11 +2,10 @@ package com.example.burrrrng.repository;
 
 import com.example.burrrrng.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    default User findByIdOrElseThrow(Long userId) {
-        return findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "없는 유저입니다."));
-    }
+
+    Optional<User> findByEmail(String email);
 }
