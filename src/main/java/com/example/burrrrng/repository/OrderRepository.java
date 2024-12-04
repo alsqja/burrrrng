2c772da9d2e7338e0d9dcd208e298c09dc538e71
alsqja <alsqha2626@gmail.com>
2024-12-04 20:53:39 +0900

    feat: 주문 내역 받기 구현
    - JPQL 로 처리가 불가능하다고 판단하고 native 쿼리 사용 후 dto 에서 매핑
    
    #22

--- src/main/java/com/example/burrrrng/repository/OrderRepository.java
+++ src/main/java/com/example/burrrrng/repository/OrderRepository.java
@@ -2,11 +2,37 @@ package com.example.burrrrng.repository;
 
 import com.example.burrrrng.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     default Order findByIdOrElseThrow(Long orderId) {
         return findById(orderId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "없는 주문 입니다."));
     }
+
+    @Query(value = """
+                SELECT o.id,
+                       s.name AS storeName,
+                       o.status AS status,
+                       (SELECT m2.name 
+                        FROM order_menu om2
+                        JOIN menu m2 ON om2.menu_id = m2.id
+                        WHERE om2.order_id = o.id
+                        ORDER BY m2.price DESC
+                        LIMIT 1) AS mainMenu,
+                       COUNT(DISTINCT om.menu_id) AS totalMenuCount,
+                       SUM(om.amount * m.price) AS totalPrice,
+                       o.created_at AS createdAt,
+                       o.updated_at AS updatedAt
+                FROM `order` o
+                JOIN store s ON o.store_id = s.id
+                JOIN order_menu om ON om.order_id = o.id
+                JOIN menu m ON m.id = om.menu_id
+                WHERE o.user_id = :id
+                GROUP BY o.id, s.id
+            """, nativeQuery = true)
+    List<Object[]> findAllUserOrders(Long id);
 }
