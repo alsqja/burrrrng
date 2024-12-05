b8220802f539256b91c8249a94cf8bc94f65b148
alsqja <alsqha2626@gmail.com>
2024-12-05 19:17:39 +0900

    feat: 메뉴 수정 테스트 코드 작성
    - 수정 여부 확인
    - 일부만 null 값 입력시 수정여부 확인

--- src/test/java/com/example/burrrrng/service/MenuServiceTest.java
+++ src/test/java/com/example/burrrrng/service/MenuServiceTest.java
@@ -1,11 +1,13 @@
 package com.example.burrrrng.service;
 
 import com.example.burrrrng.dto.RequestMenuCreateDto;
+import com.example.burrrrng.dto.RequestMenuUpdateDto;
 import com.example.burrrrng.dto.ResponseMenuDto;
 import com.example.burrrrng.dto.common.CommonResDto;
 import com.example.burrrrng.entity.Menu;
 import com.example.burrrrng.entity.Store;
 import com.example.burrrrng.entity.User;
+import com.example.burrrrng.enums.MenuStatus;
 import com.example.burrrrng.enums.StoreStatus;
 import com.example.burrrrng.enums.UserRole;
 import com.example.burrrrng.exception.SameMenuException;
@@ -16,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalTime;
 import java.util.Optional;
@@ -25,6 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@Transactional
 public class MenuServiceTest {
 
     @Autowired
@@ -47,7 +51,7 @@ public class MenuServiceTest {
 
         RequestMenuCreateDto dto = new RequestMenuCreateDto("testMenuName", 12000);
 
-        CommonResDto<ResponseMenuDto> menu = menuService.createMenu(store.getId(), dto, request);
+        CommonResDto<ResponseMenuDto> menu = menuService.createMenu(savedStore.getId(), dto, request);
 
         Optional<Menu> findMenu = menuRepository.findById(menu.getData().getId());
         SameMenuException e = assertThrows(SameMenuException.class, () -> menuService.createMenu(user.getId(), dto, request));
@@ -55,5 +59,32 @@ public class MenuServiceTest {
         assertThat(findMenu.isPresent()).isEqualTo(true);
         assertThat(findMenu.get().getName()).isEqualTo("testMenuName");
         assertThat(e.getMessage()).isEqualTo("같은 이름의 메뉴가 이미 존재합니다.");
+    }
+
+    @Test
+    void 메뉴수정_예외() {
+        User user = userRepository.save(new User("test@email.com", "0000", "testUserName", "testUserAddress", UserRole.OWNER));
+
+        MockHttpServletRequest request = getMockHttpServletRequest(user);
+
+        Store store = new Store(user, "testStoreName", LocalTime.of(10, 0), LocalTime.of(22, 0), 12000, StoreStatus.OPENED);
+        Store savedStore = storeRepository.save(store);
+
+        Menu menu = menuRepository.save(new Menu(user, savedStore, "testMenuName", 12000, MenuStatus.NORMAL));
+
+        RequestMenuUpdateDto dto = new RequestMenuUpdateDto("testPatchMenuName", 13000, MenuStatus.SOLDOUT);
+        CommonResDto<ResponseMenuDto> updatedMenu = menuService.updateMenu(savedStore.getId(), menu.getId(), dto, request);
+
+        Menu findMenu = menuRepository.findById(updatedMenu.getData().getId()).orElse(null);
+        assertThat(updatedMenu.getData().getName()).isEqualTo(findMenu.getName());
+        assertThat(updatedMenu.getData().getPrice()).isEqualTo(findMenu.getPrice());
+        assertThat(updatedMenu.getData().getStatus()).isEqualTo(findMenu.getStatus().getValue());
+
+        dto = new RequestMenuUpdateDto("testPatchMenuName2", 15000, null);
+
+        findMenu = menuRepository.findById(updatedMenu.getData().getId()).orElse(null);
+        assertThat(updatedMenu.getData().getName()).isEqualTo(findMenu.getName());
+        assertThat(updatedMenu.getData().getPrice()).isEqualTo(findMenu.getPrice());
+        assertThat(MenuStatus.SOLDOUT).isEqualTo(findMenu.getStatus());
     }
 }
